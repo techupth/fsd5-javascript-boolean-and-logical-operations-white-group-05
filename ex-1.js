87b50045841d15743f76715f77f16c3e7e333dfb
hedisnice <nicenice60225@gmail.com>
2023-11-22 13:36:01 +0700

    feat(exercise) add complete exercise

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,12 @@
 let isOver18;
 let hasCriminalBlacklist;
-
 let isAllow;
 
-// Start coding here
+//กำหนดค่าตามคุณสมบัติของ James
+isOver18 = true; // James มีอายุ 18 ปี
+hasCriminalBlacklist = false; // James ไม่เคยมีประวัติอาชญากรรม
+
+//James มีสิทธิ์เข้างานหรือไม่
+isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -6,7 +6,20 @@ let isWeekday;
 let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
-
 let hasPromotion;
+// กำหนดค่าตามคุณสมบัติของ John
+lastMonthPaidMoreThan4000 = true; // มียอดสั่งซื้อเดือนที่แล้วมากกว่า 4000 บาท
+isWeekday = true; // ไปซื้อของวันศุกร์
+hasBoughtProductFromITCategory = false; // ไม่เคยซื้อของในหมวดหมู่ IT มาก่อน
+hasAttendedDiscountEvent = true; // เคยเข้าร่วมกิจกรรมมหกรรมลดราคาครั้งที่ 2
+isPlatinum = false; // เป็น Member ระดับ Gold
+
+//คุณสมบัติของ  John มีสิทธิได้รับโปรโมชั่นส่วนลดหรือไม่
+hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    !hasBoughtProductFromITCategory &&
+    !hasAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
